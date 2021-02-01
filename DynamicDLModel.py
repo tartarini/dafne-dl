75922bceea38f6d1f3318be4ba6ef99ec3d70368
fsantini <francesco.santini@gmail.com>
2021-02-01 15:14:01 +0100

    Copyright and GPL License notice

--- DynamicDLModel.py
+++ DynamicDLModel.py
@@ -5,6 +5,21 @@ Implementation of a deep learning module that can be serialized and deserialized
 Functions for the operation of the class are provided as references to top-level functions.
 Such top level functions should define all the imports within themselves (i.e. don't put the imports at the top of the file).
 """
+#  Copyright (c) 2021 Dafne-Imaging Team
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import annotations
 from .interfaces import IncompatibleModelError, DeepLearningClass
 import dill
